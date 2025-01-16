d1f2543e2ecff47eddb6ebf7ddb309f55e5567ab
Chima-Emmanuel <emmycruz695@gmail.com>
2025-01-16 07:26:25 +0100

    fix profile skeleton error

--- app/(buyer)/(routes)/(dashboard)/settings/page.tsx
+++ app/(buyer)/(routes)/(dashboard)/settings/page.tsx
@@ -91,7 +91,7 @@ const Settings = () => {
 
 export default Settings;
 
-export const ProfileSettingsSkeleton = () => {
+const ProfileSettingsSkeleton = () => {
   return (
     <div className="max-w-full mx-auto p-4 space-y-6">
       {/* Page Title */}
